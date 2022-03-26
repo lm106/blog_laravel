16c3fdfaa72d6117f4293baa66b8998b3f0b2ad0
kiliamMartin <kmarting99@gmail.com>
2022-03-26 21:15:20 +0000

    navbar hecha

---
+++ resources/js/components/Example.vue
@@ -0,0 +1,13 @@
+<template class="borrar">
+    <h2>Hola buenas tardes</h2>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return view('navbar');
+});
 /*Route::get('/', function () {
     return view('welcome');
 });
