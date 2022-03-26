3ef9453d4a505369ba9054cf00f7ef185442104c
kiliamMartin <kmarting99@gmail.com>
2022-03-26 21:17:41 +0000

    navbar en /navbar

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
+Route::get('/navbar', function () {
     return view('navbar');
 });
 /*Route::get('/', function () {
