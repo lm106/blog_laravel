47b27aedb87dedc4f8f4f504ef35ec262895bc11
EvaBPerez <eva.correoperez@gmail.com>
2022-05-09 09:12:19 +0100

    cambiar rutas

--- resources/js/img_dir.js
+++ resources/js/img_dir.js
@@ -6,6 +6,7 @@ const dissave = 'images/dissave.png';
 const coment = 'images/coment.png';
 const user = 'images/user.png';
 const send = 'images/send.png';
+const edit = 'images/edit.png';
 export default {
-    url, like, dislike, save, dissave, coment, user, send
+    url, like, dislike, save, dissave, coment, user, send, edit
 }
