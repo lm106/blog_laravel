2d13e5abf214b62c5d11c25057a272d8e290cbc0
kiliamMartin <kmarting99@gmail.com>
2022-05-09 10:30:25 +0100

    set shorted recipes description and ingredients and added show button at recipes manager

--- resources/js/components/recipes-manager.vue
+++ resources/js/components/recipes-manager.vue
@@ -4,8 +4,8 @@
         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" style="margin-bottom:15px">Nueva receta</button>
 
         <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
+        <div class="modal fade bd-example-modal-xl" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" >
+            <div class="modal-dialog modal-xl" >
                 <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Nueva receta</h5>
@@ -19,11 +19,15 @@
                         </div>
                         <div class="mb-3">
                             <label for="recipe-description" class="col-form-label">Descripción:</label>
-                            <textarea class="form-control" id="recipe-description" v-model="recipe.description"></textarea>
+                            <textarea rows="10" class="form-control" id="recipe-description" v-model="recipe.description" style="resize:vertical"></textarea>
+                        </div>
+                        <div class="mb-3">
+                            <label for="recipe-ingredients" class="col-form-label">Ingredientes:</label>
+                            <textarea placeholder= "Introducir ingredientes separados por comas" rows="6" class="form-control" id="recipe-ingredients" v-model="recipe.ingredients " style="resize:vertical"></textarea>
                         </div>
                         <div class="mb-5">
-                            <label for="recipe-ingredients" class="col-form-label">Ingredientes:</label>
-                            <textarea class="form-control" id="recipe-ingredients" v-model="recipe.ingredients"></textarea>
+                            <label for="formFile" class="form-label">Imagen:</label>
+                            <input class="form-control" type="file" id="formFile" accept=".jpg,.png">
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
@@ -53,8 +57,9 @@
             <tr v-for="(r) in recipes" :key='r' >
                 <td scope="row">{{r.id}}</td>
                 <td scope="row">{{r.title}}</td>
-                <td scope="row">{{r.description}}</td>
-                <td scope="row">{{r.ingredients}}</td>
+                <td scope="row">{{r.short_description}}</td>
+                <td scope="row">{{r.short_ingredients}}</td>
+                <td scope="row"><button @click="showRecipe(r.id)" style="border:none; background-color:transparent"><i class="bi bi-info-circle-fill" style="color:blue" ></i></button></td>
                 <td scope="row"><button style="border:none; background-color:transparent"><i class="bi bi-pencil-fill" ></i></button></td>
                 <td scope="row"><button style="border:none; background-color:transparent"><i class="bi bi-trash3-fill" style="color:red"></i></button ></td>
             
@@ -75,6 +80,10 @@ export default {
         axios.get('/recetas')
         .then(res => { 
             this.recipes = res.data
+            for(var i=0; i < this.recipes.length; i++){
+                this.recipes[i].short_description = this.recipes[i].description.substring(0,200) + "..."
+                this.recipes[i].short_ingredients = this.recipes[i].ingredients.substring(0,200) + "..."
+            }
         }),
 
         
@@ -99,6 +108,8 @@ export default {
                 image: '',
                 ingredients: '',
                 description: '',
+                short_description: '',
+                short_ingredients: '',
                 user_id: ''
                 }],
             user: [],
@@ -115,7 +126,7 @@ export default {
             axios.post('/new_recipe', {title: this.recipe.title, image: "borrar", description: this.recipe.description, ingredients: this.recipe.ingredients, user_id: this.user[0].id}).then(() => {
             console.log('saved');
             window.location.href="/recipes_manager";
-            console.log(this.recipe[0])
+            console.log(this.recipe[0].description)
           }, function (error) {
             console.log(error.response.data); 
             
@@ -124,6 +135,9 @@ export default {
         test(){
            console.log(this.user[0].id)
         },
+        showRecipe(id){
+            window.location.href = '/recipe_description/' + id
+        }
 
     }
 }
