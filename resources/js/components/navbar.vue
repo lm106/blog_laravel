9eaaac334c02576314195f0e197129bef38eb21d
lm106 <>
2022-05-10 23:58:28 +0100

    correción del length

--- resources/js/components/navbar.vue
+++ resources/js/components/navbar.vue
@@ -10,7 +10,7 @@
             <router-link to="/" id="myTitle">Blog de Recetas</router-link>
               
           </div>
-          <div v-if="user.length < 1" class="no-user text-end position-absolute top-0 end-0" id="menu_login">
+          <div v-if="!user" class="no-user text-end position-absolute top-0 end-0" id="menu_login">
             <router-link to="/login" id="log-in">Iniciar sesión</router-link>
           </div>
           <div v-else-if="userType == 1">
