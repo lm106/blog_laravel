21f42ad89b439f1e230cdd2510af5f0de6e2aa36
Cesar Delgado <cjdsgithub@gmail.com>
2022-05-15 21:21:49 +0100

    modal list_user de eliminar receta a usuario

--- resources/js/components/user/admin/list_user.vue
+++ resources/js/components/user/admin/list_user.vue
@@ -30,7 +30,7 @@
             <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLongTitle">Borrar receta</h5>
+                    <h5 class="modal-title" id="exampleModalLongTitle">Borrar usuario</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
